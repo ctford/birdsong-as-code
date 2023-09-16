1944994ea596bb52d2dd29a8dbccb646287cfe01
Chris Ford <christophertford@gmail.com>
2023-09-16 15:10:14 +0200

    Show how modelling is understanding.

--- src/birdsong_as_code/song.clj
+++ src/birdsong_as_code/song.clj
@@ -144,13 +144,13 @@
 ;;; Representation matters ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(def raw [1 2 2 3 3 3 4 4 4 4 5 5 5 5 5 6 6 6 6 6 6 7 7 7 7 7 7 7])
 
-
-
-
-
-
-
+(def computed
+  (->>
+    (range 1 8)
+    (map #(repeat % %))
+    (reduce concat))
 
 
 
